b74ce3e5ea02d6fa878c8d696644a76f8af55146
Green Code <greencode.org@gmail.com>
2022-12-06 12:05:19 +0000

    Updated Version doubletsScanpy

--- doubletsScanpy.py
+++ doubletsScanpy.py
@@ -19,9 +19,23 @@ sc.settings.verbosity = 3  # verbosity: errors (0), warnings (1), info (2), hint
 
 inputData = "./dataSaveOriginal/rawDataset.h5ad" # ./dataSaveOriginal/rawDataset5000.h5ad
 
-adata = sc.read("./output/savedDataClustersFinal.h5ad")
+adata = sc.read("./dataSaveOriginal/rawDataset.h5ad")
 adata.var_names_make_unique()
 adata.X = adata.X.astype('float64')
+
+adataCON_DS2U =  adata[adata.obs["sample"] == "CON_DS2U"]
+adataCON_H9 =  adata[adata.obs["sample"] == "CON_H9"]
+adataCON_IMR =  adata[adata.obs["sample"] == "CON_IMR"]
+adataCON_ihtc =  adata[adata.obs["sample"] == "CON_ihtc"]
+
+adataDS_2DS3 =  adata[adata.obs["sample"] == "DS_2DS3"]
+adataDS_DS1 =  adata[adata.obs["sample"] == "DS_DS1"]
+adataDS_DSP =  adata[adata.obs["sample"] == "DS_DSP"]
+
+adata = adataCON_DS2U.concatenate(adataCON_H9)
+adata = adata.concatenate(adataCON_IMR)
+adata = adata.concatenate(adataDS_2DS3)
+adata = adata.concatenate(adataDS_DSP)
 
 print(adata)
 
@@ -44,28 +58,28 @@ adata.var['ribo'] = adata.var_names.isin(riboGenes[0].values)
 # Calculate QC Metrics
 sc.pp.calculate_qc_metrics(adata, qc_vars=['mt', "ribo"], percent_top=None, log1p=False, inplace=True)
 
-print(adata)
-sc.external.pp.scrublet(adata)
-sc.external.pl.scrublet_score_distribution(adata)
-
-print(adata.obs)
-sc.pp.normalize_total(adata, target_sum=10000)
-sc.pp.log1p(adata)
-sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
-
-adata.raw = adata
-adata = adata[:, adata.var.highly_variable]
-
-sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt'])
-sc.pp.scale(adata, max_value=10)
-sc.tl.pca(adata, svd_solver='arpack')
-sc.pp.neighbors(adata, n_neighbors=15, n_pcs=40) # n_neighbors=15 is default
-sc.tl.umap(adata)
-sc.tl.leiden(adata, resolution=0.3)
-
 with PdfPages('./outputPDFs/Doublets Plots.pdf') as pdf:
     dummyPlot = plt.plot([1, 2])
     figureNum = plt.gcf().number
+
+    print(adata)
+    sc.external.pp.scrublet(adata)
+    sc.external.pl.scrublet_score_distribution(adata, show=False)
+
+    print(adata.obs)
+    sc.pp.normalize_total(adata, target_sum=10000)
+    sc.pp.log1p(adata)
+    sc.pp.highly_variable_genes(adata, min_mean=0.0125, max_mean=3, min_disp=0.5)
+
+    adata.raw = adata
+    adata = adata[:, adata.var.highly_variable]
+
+    sc.pp.regress_out(adata, ['total_counts', 'pct_counts_mt'])
+    sc.pp.scale(adata, max_value=10)
+    sc.tl.pca(adata, svd_solver='arpack')
+    sc.pp.neighbors(adata, n_neighbors=15, n_pcs=40) # n_neighbors=15 is default
+    sc.tl.umap(adata)
+    sc.tl.leiden(adata, resolution=0.3)
 
     # Pre-Filtering
     sc.pl.umap(adata, color='doublet_score', show=False)
@@ -78,6 +92,9 @@ with PdfPages('./outputPDFs/Doublets Plots.pdf') as pdf:
     adata = adata[adata.obs.n_genes_by_counts > 1000, :]
 
     # Post-filtering
+    sc.pl.umap(adata, color='doublet_score', show=False)
+    adata.obs['predicted_doublet'] = adata.obs['predicted_doublet'].astype(str).astype('category')
+    sc.pl.umap(adata, color='predicted_doublet', show=False)
 
     for fig in range(figureNum+1,  plt.gcf().number+1):
         pdf.savefig(figure=fig, bbox_inches='tight')
